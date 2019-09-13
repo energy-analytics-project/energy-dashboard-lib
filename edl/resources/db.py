9d5b71119e6cbd1d78356606ba95370e10ae8816
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-09-13 07:06:05 -0700

    add mo-better logging to resources/db.py

--- edl/resources/db.py
+++ edl/resources/db.py
@@ -6,25 +6,58 @@ from edl.resources import log
 
 def insert(logger, resource_name, sql_dir, db_dir, db_name, new_files):
     chlogger = logger.getChild(__name__)
+    new_files_count = len(new_files)
+    log.info(chlogger, {
+        "name"      : __name__,
+        "src"       : resource_name, 
+        "method"    : "insert",
+        "sql_dir"   : sql_dir,
+        "db_dir"    : db_dir,
+        "db_name"   : db_name,
+        "new_files" : new_files_count,
+        })
     with sqlite3.connect(os.path.join(db_dir, db_name)) as cnx:
-        for new_file in new_files:
+        for (idx, new_file) in enumerate(new_files):
             sqlfile = os.path.join(sql_dir, new_file) 
             try:
                 with open(sqlfile, 'r') as sf:
+                    log.debug(chlogger, {
+                        "name"      : __name__,
+                        "src"       : resource_name, 
+                        "method"    : "insert",
+                        "sql_dir"   : sql_dir,
+                        "db_dir"    : db_dir,
+                        "db_name"   : db_name,
+                        "new_files" : new_files_count,
+                        "file_idx"  : idx,
+                        "sql_file"  : sqlfile,
+                        "message"   : "started",
+                        })
                     cnx.executescript(sf.read())
-                    log.info(chlogger, {
-                        "name"  : __name__,
-                        "src"   :resource_name, 
-                        "method":"insert",
-                        "file"  :sqlfile,
+                    log.debug(chlogger, {
+                        "name"      : __name__,
+                        "src"       : resource_name, 
+                        "method"    : "insert",
+                        "sql_dir"   : sql_dir,
+                        "db_dir"    : db_dir,
+                        "db_name"   : db_name,
+                        "new_files" : new_files_count,
+                        "file_idx"  : idx,
+                        "sql_file"  : sqlfile,
+                        "message"   : "completed",
                         })
                 yield sqlfile
             except Exception as e:
                 log.error(chlogger, {
-                    "name"  : __name__,
-                    "src"   :resource_name, 
-                    "method":"insert",
-                    "file"  :sqlfile,
+                    "name"      : __name__,
+                    "src"       : resource_name, 
+                    "method"    : "insert",
+                    "sql_dir"   : sql_dir,
+                    "db_dir"    : db_dir,
+                    "db_name"   : db_name,
+                    "new_files" : new_files_count,
+                    "file_idx"  : idx,
+                    "sql_file"  : sqlfile,
                     "ERROR":"insert failed",
-                    "exception": str(e)
+                    "exception": str(e),
                     })
