77bc42cc18a4453291615c6b9c6b7b01384ea3bf
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-09-16 16:07:12 -0700

    add gpl3 license snippet to code

--- edl/templates/src/20_unzp.py
+++ edl/templates/src/20_unzp.py
@@ -1,4 +1,20 @@
 #! /usr/bin/env python3
+# edl : common library for the energy-dashboard tool-chain
+# Copyright (C) 2019  Todd Greenwood-Geer (Enviro Software Solutions, LLC)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 # -----------------------------------------------------------------------------
 # 20_unzp.py : unzip zip files in ZIP_DIR to XML_DIR in preparation for
