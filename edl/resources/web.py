26c76ec526de01b91145f82b46cc6c726aefbd61
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-09-22 13:23:07 -0700

    remove timeout as caiso requests are timing out unnecessarily

--- edl/resources/web.py
+++ edl/resources/web.py
@@ -76,8 +76,7 @@ def download(logger, resource_name, delay, urls, state_file, path):
                 # update the state_file with files that were found on disk
                 downloaded.append(url)
                 continue
-            # url does not exist in manifest and the file does not exist on disk, download it
-            r = requests.get(url, verify=False, timeout=1)
+            r = requests.get(url)
             if r.status_code == 200:
                 with open(os.path.join(path, filename), 'wb') as fd:
                     for chunk in r.iter_content(chunk_size=128):
