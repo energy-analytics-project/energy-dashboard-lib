00ce6fd12810f5b810362ea3fdfbd237a70b66e6
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-09-23 12:03:30 -0700

    remove verbose flag from shell file copy

--- edl/templates/src/60_dist.sh
+++ edl/templates/src/60_dist.sh
@@ -6,7 +6,7 @@
 
 mkdir -p ./dist/zip
 mkdir -p ./dist/db
-cp -rv ./zip/*.zip ./dist/zip/.
-cp -rv ./zip/state.txt ./dist/zip/.
-cp -v ./db/*.db ./dist/db/.
+cp -r ./zip/*.zip ./dist/zip/.
+cp -r ./zip/state.txt ./dist/zip/.
+cp ./db/*.db ./dist/db/.
 pigz ./dist/db/*.db
